0af000890ee9796acfbd92f9edaeb7899da80736
Xiaokui Shu <subbyte@gmail.com>
2015-12-21 20:06:47 -0500

    update:     1) handles multiple footnotes     2) kill the curly bracket warning for perl 5.22

--- tex2text.sh
+++ tex2text.sh
@@ -3,7 +3,7 @@
 #           tex2text: Interpreting Tex into Plaintext
 #
 #   author:     Xiaokui Shu
-#   version:    1.0.1
+#   version:    1.1.0
 #   license:    Apache 2.0
 #   email:      subx@cs.vt.edu
 ################################################################
@@ -14,12 +14,14 @@ ofile=${1%.*}.txt
 
 echo "interpreting $1 into plaintext file $ofile..."
 
+
 #### functions ####
 removespace () {
-    perl -p -i -e 's/[ \t]+/ /g' $ofile
-    perl -p -i -e 's/^ //g' $ofile
-    perl -p -i -e 's/ $//g' $ofile
+    perl -i -ple 's/\s+/ /g' $ofile
+    perl -i -pe 's/^ //g' $ofile
+    perl -i -pe 's/ $//g' $ofile
 }
+
 
 #### Sec 1 ####
 # init and change format
@@ -27,100 +29,102 @@ cp $1 $ofile
 dos2unix -q $ofile
 
 # replace "~" with space
-perl -p -i -e 's/~/ /g' $ofile
+perl -i -pe 's/~/ /g' $ofile
 
 # remove all comments
+# remove whole-line comment
 sed -i '/^\s*%/d' $ofile
-perl -p -i -e 's/([^\\])%(.*)$/\1/g' $ofile
+# remove inline comment, (skip "\%", which is a percetage sign)
+perl -i -pe 's/([^\\])%(.*)$/\1/g' $ofile
+# remove ifFull-fi sections
+sed -i '/\\ifFull/,/\\fi/d' $ofile
 
 removespace
 
 # join lines in each paragraph and get rid of multiple empty lines
-perl -00pl -i -e 's/\s*\n\s*/ /g' $ofile
+perl -i -00ple '' $ofile
 
 
 #### Sec 2 ####
 # remove escape before "%"
-perl -p -i -e 's/\\%/%/g' $ofile
+perl -i -pe 's/\\%/%/g' $ofile
 
 # handle quotes
-perl -p -i -e "s/\`\`([^']+)''/\"\1\"/g" $ofile
+perl -i -pe "s/\`\`([^']+)''/\"\1\"/g" $ofile
 
 # remove all citations
-perl -p -i -e 's/(e\.g\.,|, e\.g\.,|in)?( |~)?\\cite{[^}]+}//g' $ofile
+perl -i -pe 's/(e\.g\.,|, e\.g\.,|in)?( |~)?\\cite\{[^\}]+\}//g' $ofile
 
 # replace reference with "9"
-perl -p -i -e 's/\\(ref){[^}]+}/9/g' $ofile
+perl -i -pe 's/\\ref\{[^\}]+\}/9/g' $ofile
 
 # replace \verb with "X"
-perl -p -i -e 's/\\verb(.)([^\\1]+)\1/X/g' $ofile
+perl -i -pe 's/\\verb(.)([^\\1]+)\1/X/g' $ofile
 
 # open abstract
-perl -p -i -e 's/\\begin{(abstract)}//g' $ofile
-perl -p -i -e 's/\\end{(abstract)}//g' $ofile
+perl -i -pe 's/\\(begin|end)\{(abstract)\}//g' $ofile
 
 # open titles and remove labels
-perl -p -i -e 's/\\(section|subsection|subsubsection){([^}]+)}/\2/g' $ofile
-perl -p -i -e 's/\\label{[^}]+}//g' $ofile
+perl -i -pe 's/\\(section|subsection|subsubsection)\{([^\}]+)\}/\2/g' $ofile
+perl -i -pe 's/\\label\{[^\}]+\}/ /g' $ofile
 
 # remove "\noindent"
-perl -p -i -e 's/\\noindent/ /g' $ofile
+perl -i -pe 's/\\noindent/ /g' $ofile
 
 # remove fontsizes
-perl -p -i -e 's/\\(tiny|scriptsize|footnotesize|small)/ /g' $ofile
+perl -i -pe 's/\\(tiny|scriptsize|footnotesize|small)/ /g' $ofile
 
 # remove "\vspace", "\hspace"
-perl -p -i -e 's/\\[vh]space{[^}]+}/ /g' $ofile
+perl -i -pe 's/\\[vh]space\{[^\}]+\}/ /g' $ofile
 
 # remove enumerate env
-perl -p -i -e 's/\\begin{(enumerate|itemize|description|inparaenum|svgraybox)}(\[[^\]]+\])?//g' $ofile
-perl -p -i -e 's/\\end{(enumerate|itemize|description|inparaenum|svgraybox)}//g' $ofile
-perl -p -i -e 's/\\item //g' $ofile
-perl -p -i -e 's/ (i|ii|iii|iv|v|vi|vii|viii|iv|x)\) / /g' $ofile
-
-# open ifFull
-perl -p -i -e 's/\\ifFull//g' $ofile
-perl -p -i -e 's/\\fi//g' $ofile
+perl -i -pe 's/\\begin\{(enumerate|itemize|description|inparaenum|svgraybox)\}(\[[^\]]+\])?//g' $ofile
+perl -i -pe 's/\\end\{(enumerate|itemize|description|inparaenum|svgraybox)\}//g' $ofile
+perl -i -pe 's/\\item //g' $ofile
+perl -i -pe 's/ (i|ii|iii|iv|v|vi|vii|viii|iv|x)\) / /g' $ofile
 
 # open definition, theorem, lamma, example, proof
-perl -p -i -e 's/\\begin{(definition|theorem|lemma|example|proof)}//g' $ofile
-perl -p -i -e 's/\\end{(definition|theorem|lemma|example|proof)}//g' $ofile
+perl -i -pe 's/\\begin\{(definition|theorem|lemma|example|proof)\}//g' $ofile
+perl -i -pe 's/\\end\{(definition|theorem|lemma|example|proof)\}//g' $ofile
+
+# remove escaped $
+perl -i -pe 's/\\\$//g' $ofile
 
 # put equation into $...$ for further deleting
-perl -p -i -e 's/\\begin{equation}/\$/g' $ofile
-perl -p -i -e 's/\\end{equation}/\$/g' $ofile
+perl -i -pe 's/\\begin\{equation\}/\$/g' $ofile
+perl -i -pe 's/\\end\{equation\}/\$/g' $ofile
 
 # replace math symbols
-perl -p -i -e 's/\$n\$/n/g' $ofile
-perl -p -i -e 's/\$[^\$]+\$/X/g' $ofile
-perl -p -i -e 's/X$//g' $ofile
-
-# handle escaped $
-perl -p -i -e 's/\\\$/\$/g' $ofile
+perl -i -pe 's/\$n\$/n/g' $ofile
+perl -i -pe 's/\$[^\$]+\$/X/g' $ofile
 
 # unformat it, bf, sc, tt
-perl -p -i -e 's/\\text(it|bf|sc|tt){([^}]+)}/\2/g' $ofile
-perl -p -i -e 's/\\emph{([^}]+)}/\1/g' $ofile
-perl -p -i -e 's/{\\(em|emph|bf|sc|tt) ([^}]+)}/\2/g' $ofile
+perl -i -pe 's/\\text(it|bf|sc|tt)\{([^\}]+)\}/\2/g' $ofile
+perl -i -pe 's/\\emph\{([^\}]+)\}/\1/g' $ofile
+perl -i -pe 's/\{\\(em|emph|bf|sc|tt) ([^\}]+)\}/\2/g' $ofile
 
 # dots
-perl -p -i -e 's/\\dots/\.\.\./g' $ofile
+perl -i -pe 's/\\dots/\.\.\./g' $ofile
 
 
 #### Sec 3: steps that require inner {} to be cleaned first ####
-# put footnote at the end of a line.
-perl -p -i -e 's/^(.*)\\footnote{([^}]+)}(.*)$/\1\3 \2/g' $ofile
+# put all footnotes at the end of a line.
+while grep -q "\\\\footnote" $ofile
+do
+    perl -i -pe 's/^(.*)\\footnote\{([^\}]+)\}(.*)$/\1\3 \2/g' $ofile
+done
 
 # extract captions from figures and tables
-perl -p -i -e 's/^\\begin{([^}]+)}(.*)\\caption{([^}]+)}(.*)\\end{\1}/\3/g' $ofile
+perl -i -pe 's/^\\begin\{([^\}]+)\}(.*)\\caption\{([^\}]+)\}(.*)\\end\{\1\}/\3/g' $ofile
 
 # remove empty begin-end/parentheses pairs
-perl -p -i -e 's/\\begin{([^}]+)}( +)\\end{\1}/ /g' $ofile
-perl -p -i -e 's/ \(\)//g' $ofile
+perl -i -pe 's/\\begin\{([^\}]+)\}( +)\\end\{\1\}/ /g' $ofile
+perl -i -pe 's/ \(\)//g' $ofile
 
 
 #### Sec 4: final adjustment ####
 removespace
-perl -p -i -e 's/ \./\./g' $ofile
-perl -p -i -e 's/, , /, /g' $ofile
-perl -00 -p -i -e '' $ofile
+
+perl -i -pe 's/ \./\./g' $ofile
+perl -i -pe 's/, , /, /g' $ofile
+perl -i -00ple '' $ofile
